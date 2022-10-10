04c7217e066107246a7b988136791233f753296c
ShorenaK <shorenaanzhilov@gmail.com>
2022-10-10 12:42:07 -0400

    super user created

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,13 @@
+from email.policy import default
+from enum import unique
 from django.db import models
-
+import uuid
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_Length = 200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_Length = 2000, null=True, blank=True)
+    source_link = models.CharField(max_Length = 2000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                         primary_key=True, editable=False)
