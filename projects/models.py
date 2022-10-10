c4c41707181b19a21a992a3540bd16f03f1d808e
ShorenaK <shorenaanzhilov@gmail.com>
2022-10-10 14:19:47 -0400

    review's model created

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,7 @@
 from email.policy import default
 from enum import unique
+from multiprocessing.sharedctypes import Value
+from secrets import choice
 from django.db import models
 import uuid
 # Create your models here.
@@ -14,3 +16,16 @@ class Project(models.Model):
 
     def __str__(self):
        return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Up Down'),
+    )
+        # owner =
+        # project =
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length = 200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                         primary_key=True, editable=False)
