e210fae6776a8529562616818db35c099a9bc3b6
ShorenaK <shorenaanzhilov@gmail.com>
2022-10-10 19:38:40 -0400

    finally delete route is working full CRUD for Project routes

--- projects/views.py
+++ projects/views.py
@@ -45,7 +45,7 @@ def updateProject(request, pk):
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
-        project.delete('projects')
-        redirect()
+        project.delete()
+        return redirect('projects')
     context = {'object' : project}
     return render(request, "projects/delete_template.html", context)
