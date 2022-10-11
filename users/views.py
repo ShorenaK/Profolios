3301fb3b5e4b7eb73fb93acaae04cc8f8f6a809a
ShorenaK <shorenaanzhilov@gmail.com>
2022-10-11 13:50:08 -0400

    user_profily styling

--- users/views.py
+++ users/views.py
@@ -10,6 +10,11 @@ def profiles(request):
 
 
 def userProfile(request, pk):
-    # profiles = Profile.objects.all()
-    # context = {'profiles': profiles}
-    return render(request, 'users/user-profile.html')
+    profile = Profile.objects.get(id=pk)
+   
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+
+    context = {'profile': profile, 'topSkills': topSkills,
+               "otherSkills": otherSkills}
+    return render(request, 'users/user-profile.html', context)
