67cbe333a70bef55df73b533b58c4b2c39676b65
ShorenaK <shorenaanzhilov@gmail.com>
2022-10-12 10:46:53 -0400

    delete skill created

--- projects/views.py
+++ projects/views.py
@@ -57,4 +57,4 @@ def deleteProject(request, pk):
         project.delete()
         return redirect('projects')
     context = {'object' : project}
-    return render(request, "projects/delete_template.html", context)
+    return render(request, "delete_template.html", context)
