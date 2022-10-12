327960712acfbf535606029873004ee25468e2da
ShorenaK <shorenaanzhilov@gmail.com>
2022-10-11 22:25:20 -0400

    account page createad

--- users/views.py
+++ users/views.py
@@ -83,3 +83,12 @@ def userProfile(request, pk):
                "otherSkills": otherSkills}
     return render(request, 'users/user-profile.html', context)
 
+
+
+
+def userAccount(request):
+    profile = request.user.profile
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+    context = {'profile': profile, 'skills': skills, 'projects': projects}
+    return render(request, 'users/account.html', context)
