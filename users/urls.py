35d97d5a46b100bbe18660843c97db137fdcea0a
ShorenaK <shorenaanzhilov@gmail.com>
2022-10-12 10:29:07 -0400

    url path craeted for update-skill

--- users/urls.py
+++ users/urls.py
@@ -15,6 +15,6 @@ urlpatterns =[
     path('edit-account/', views.editAccount, name="edit-account"),
 
     path('create-skill/', views.createSkill, name="create-skill"),
-    
+    path('update-skill/<str:pk>/', views.updateSkill, name="update-skill"),
     
 ]
